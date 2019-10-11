6063fed614e3ae9f619c5823d15e0abe035a1ad4
eggbread <gyeongmin251@gmail.com>
2019-10-11 20:12:19 +0900

    1

---
+++ ARPLayer.java
@@ -0,0 +1,182 @@
+package ipc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+
+public class ARPLayer implements BaseLayer {
+    public int nUpperLayerCount = 0;
+    public String pLayerName = null;
+    public BaseLayer p_UnderLayer = null;
+    public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<>();
+    public HashMap<byte[], byte[]> arp_table = new HashMap<>();
+	public _ARP_HEADER arp_Header = new _ARP_HEADER();
+	public HashMap<byte[], byte[]> proxy_table = new HashMap<>();
+
+    private class _ARP_ADDR {
+        private byte[] mac_addr = new byte[6];
+        private byte[] ip_addr = new byte[4];
+
+        public _ARP_ADDR() {
+            for (int indexOfAddr = 0; indexOfAddr < mac_addr.length; ++indexOfAddr) {
+                this.mac_addr[indexOfAddr] = (byte) 0x00;
+            }
+            for (int indexOfAddr = 0; indexOfAddr < ip_addr.length; ++indexOfAddr) {
+                this.ip_addr[indexOfAddr] = (byte) 0x00;
+            }
+        }
+    }
+	private class _ARP_HEADER {
+		byte is_checked;//arp이면 06 ip이면 08
+        byte[] arp_mac_type;
+        byte[] arp_ip_type;
+        byte arp_mac_addr_len;
+        byte arp_ip_addr_len;
+        byte[] arp_opcode;
+        _ARP_ADDR arp_srcaddr;
+        _ARP_ADDR arp_dstaddr;
+
+        public _ARP_HEADER() {
+           this.is_checked=0x00;
+           this.arp_mac_type = new byte[2];
+           this.arp_ip_type = new byte[2];
+           this.arp_mac_addr_len = 0x06;
+           this.arp_ip_addr_len = 0x04;
+           this.arp_opcode = new byte[2];
+           this.arp_srcaddr = new _ARP_ADDR();
+           this.arp_dstaddr = new _ARP_ADDR();
+        }
+    }//내부 클래스
+    public boolean containMacAddress(byte[] input) {
+    	return this.arp_table.containsKey(input);
+    }
+    public byte[] getMacAddress(byte[] input) {
+    	return this.arp_table.get(input);
+    }
+	@Override
+	public synchronized boolean Send(byte[] input, int length) {
+		//IPLayer로부터 ARP를 요청받음
+		//ARP를 만든다.
+		byte[] headerAddedArray=ObjToByte_Send(arp_Header, (byte)0x06,(byte)0x01);
+		
+		
+//		if(arp_table.containsKey(dst_Ip)) {//아는경우 ethernet type 0800 헤더생성 
+//			headerAddedArray=input;
+////			((EthernetLayer)this.GetUnderLayer()).setDstAddress(dst_Ip);
+////			return this.GetUnderLayer().Send(headerAddedArray,)
+//		}else {//모르는 경우 0806 상대 mac주소를 모르는 헤더 생성
+//			headerAddedArray=this.ObjToByte_Send(arp_Header, input);
+//		}
+		
+		this.GetUnderLayer().Send(headerAddedArray, headerAddedArray.length);
+		//EthernetLayer의 send호출
+		return false;
+		
+	}
+	
+	@Override
+	public synchronized boolean Receive(byte[] input) {
+		byte is_checked = input[0];
+		byte[] hardware_type=Arrays.copyOfRange(input, 1,3);
+		byte[] protocol_type=Arrays.copyOfRange(input, 3,5);
+		byte length_mac_address = input[5];
+		byte length_ip_address = input[6];
+		byte[] opcode = Arrays.copyOfRange(input, 7, 9);
+		byte[] src_mac_address=Arrays.copyOfRange(input, 9, 15);
+		byte[] src_ip_address=Arrays.copyOfRange(input, 15, 19);
+		byte[] dst_mac_address=new byte[6];//내 Mac주소 ****고쳐야함****
+		byte[] dst_ip_address=Arrays.copyOfRange(input, 25, 29);
+		
+		if(opcode[0]==0x00&opcode[1]==0x01) {//ARP 요청 받음
+			this.arp_table.put(src_ip_address, src_mac_address);//table에 추가
+			if(dst_ip_address==this.arp_Header.arp_srcaddr.ip_addr) {//내 ip로 옴
+				_ARP_HEADER response_header = new _ARP_HEADER();//보낼 헤드
+				response_header.arp_srcaddr.mac_addr=dst_mac_address;
+				response_header.arp_srcaddr.ip_addr=dst_ip_address;
+				response_header.arp_dstaddr.mac_addr=src_mac_address;
+				response_header.arp_dstaddr.ip_addr=src_ip_address;
+				byte[] response_arp=ObjToByte_Send(response_header, (byte)0x06, (byte)0x02);
+				this.GetUnderLayer().Send(response_arp,response_arp.length);
+			}else {//내 ip로 안옴
+				if(this.proxy_table.containsKey(dst_ip_address)) {//연결된 proxy이다 ****여기부터 해라*****
+					dst_mac_address=this.proxy_table.get(dst_ip_address);
+				}else {//proxy아님
+					
+				}
+			}
+		}else if(opcode[0]==0x00&opcode[1]==0x02) {//요청이 돌아옴
+			this.arp_table.put(src_ip_address, src_mac_address);//요청받은거 테이블에 저장
+		}
+		return false;
+	}
+	public byte[] ObjToByte_Send(_ARP_HEADER Header,byte is_checked,byte opcode) {
+	      byte[] buf = new byte[29];
+	      byte[] src_mac = Header.arp_srcaddr.mac_addr;
+	      byte[] src_ip = Header.arp_srcaddr.ip_addr;
+	      byte[] dst_mac = Header.arp_dstaddr.mac_addr;
+	      byte[] dst_ip = Header.arp_dstaddr.ip_addr;
+
+	      buf[0] = is_checked;
+	      buf[1] = 0x00;
+	      buf[2] = 0x01;//Hard
+	      buf[3] = 0x08;
+	      buf[4] = 0x00;//protocol
+	      buf[5] = Header.arp_mac_addr_len;//1바이트
+	      buf[6] = Header.arp_ip_addr_len;//2바이트
+	      buf[7] = 0x00;
+	      buf[8] = opcode;
+	      System.arraycopy(src_mac, 0, buf, 9, 6);//6바이트
+	      System.arraycopy(src_ip, 0, buf, 15, 4);//4바이트
+	      System.arraycopy(dst_mac, 0, buf, 19, 6);//6바이트
+	      System.arraycopy(dst_ip, 0, buf, 25, 4);//4바이트
+	      
+
+	      return buf;
+	   }
+    public ARPLayer(String name) {
+    	this.pLayerName = name;
+    }
+
+
+    @Override
+    public String GetLayerName() {
+        return pLayerName;
+    }
+
+    @Override
+    public BaseLayer GetUnderLayer() {
+        if (p_UnderLayer == null)
+            return null;
+        return p_UnderLayer;
+    }
+
+    @Override
+    public BaseLayer GetUpperLayer(int nindex) {
+        if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+            return null;
+        return p_aUpperLayer.get(nindex);
+    }
+
+    @Override
+    public void SetUnderLayer(BaseLayer pUnderLayer) {
+        if (pUnderLayer == null)
+            return;
+        this.p_UnderLayer = pUnderLayer;
+    }
+
+    @Override
+    public void SetUpperLayer(BaseLayer pUpperLayer) {
+        if (pUpperLayer == null)
+            return;
+        this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);//layer異붽�
+    }
+
+    @Override
+    public void SetUpperUnderLayer(BaseLayer pUULayer) {
+        this.SetUpperLayer(pUULayer);
+        pUULayer.SetUnderLayer(this);
+    }
+
+}
---
+++ IPLayer.java
@@ -0,0 +1,193 @@
+package ipc;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class IPLayer implements BaseLayer {
+    public int nUpperLayerCount = 0;
+    public String pLayerName = null;
+    public BaseLayer p_UnderLayer = null;
+    public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+    _IP_Header ip_header = new _IP_Header();
+
+    private static LayerManager m_LayerMgr = new LayerManager();
+
+    public IPLayer(String pName) {
+        pLayerName = pName;
+
+        SetIpSrcAddress(GetIpAddress());//내 IP주소 세팅
+    }
+
+    public byte[] RemoveCappHeader(byte[] input, short length) {
+
+        byte[] temp = new byte[length - 20];
+        System.arraycopy(input, 20, temp, 0, length - 20);
+        return temp;
+    }
+
+    public boolean Send(byte[] input, short length) {
+        byte[] temp = ObjToByte20(this.ip_header, input, length);
+
+        return this.GetUnderLayer().Send(temp, length + 20);    //ARP Layer에게 전달
+    }
+
+    public byte[] ObjToByte20(_IP_Header ip_header, byte[] input, short length) {
+
+        byte[] buf = new byte[length + 20];
+
+        buf[0] = ip_header.ip_verlen;
+        buf[1] = ip_header.ip_tos;
+        buf[2] |= (byte) ((length >> 8) & 0xFF);
+        buf[3] |= (byte) (length & 0xFF);
+
+        buf[4] |= (byte) ((ip_header.ip_id >> 8) & 0xFF);
+        buf[5] |= (byte) (ip_header.ip_id & 0xFF);
+
+        buf[6] |= ((ip_header.ip_fragoff >> 8) & 0xFF);
+        buf[7] |= (ip_header.ip_fragoff & 0xFF);
+
+        buf[8] = ip_header.ip_ttl;
+        buf[9] = ip_header.ip_proto;
+
+        buf[10] |= ((ip_header.ip_cksum >> 8) & 0xFF);
+        buf[11] |= (ip_header.ip_cksum & 0xFF);
+
+        System.arraycopy(ip_header.ip_srcaddr, 0, buf, 12, 4);
+        System.arraycopy(ip_header.ip_dstaddr, 0, buf, 16, 4);
+
+        return buf;
+    }
+
+    public synchronized boolean Receive(byte[] input) {
+        // IP 타입 체크
+        if (this.ip_header.ip_verlen != input[0] || this.ip_header.ip_tos != input[1]
+                || this.ip_header.ip_id != (short) ((input[4] & 0xFF) << 8 + input[5] & 0xFF)
+                || this.ip_header.ip_proto != input[9])
+            return false;
+
+
+        for (int i = 0; i < 4; i++)
+            if (this.ip_header.ip_srcaddr.addr[i] != input[16 + i]) { //input의 target IP주소가 나의 IP주소와 일치하는지 확인
+                //return this.GetUnderLayer().proxySend(input, tot_len); //일치하지 않으면 프록시
+            }
+
+        //일치하면 demultiplex하고 상위 레이어로 올림
+        short tot_len = (short) (((input[2] & 0xFF) << 8) + input[3] & 0xFF);
+        byte[] temp = RemoveCappHeader(input, tot_len);
+        return this.GetUpperLayer(0).Receive(temp);
+    }
+
+    @Override
+    public String GetLayerName() {
+        return pLayerName;
+    }
+
+    @Override
+    public BaseLayer GetUnderLayer() {
+        if (p_UnderLayer == null)
+            return null;
+        return p_UnderLayer;
+    }
+
+    @Override
+    public BaseLayer GetUpperLayer(int nindex) {
+        if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+            return null;
+        return p_aUpperLayer.get(nindex);
+    }
+
+    @Override
+    public void SetUnderLayer(BaseLayer pUnderLayer) {
+        if (pUnderLayer == null)
+            return;
+        this.p_UnderLayer = pUnderLayer;
+    }
+
+    @Override
+    public void SetUpperLayer(BaseLayer pUpperLayer) {
+        // TODO Auto-generated method stub
+        if (pUpperLayer == null)
+            return;
+        this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
+
+    }
+
+    @Override
+    public void SetUpperUnderLayer(BaseLayer pUULayer) {
+        this.SetUpperLayer(pUULayer);
+        pUULayer.SetUnderLayer(this);
+    }
+
+    // src IP주소 세팅
+    public void SetIpSrcAddress(byte[] srcAddress) {
+        ip_header.ip_srcaddr.addr = srcAddress;
+    }
+
+    // dst IP주소 세팅
+    public void SetIpDstAddress(byte[] dstAddress) {
+        ip_header.ip_dstaddr.addr = dstAddress;
+
+    }
+
+    public byte[] GetIpAddress() {
+
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String[] hostAddressInString = local.getHostAddress().split("[.]");
+
+        byte[] temp = new byte[4];
+        for (int i = 0; i < 4; i++)
+            temp[i] = Byte.parseByte(hostAddressInString[i]);
+
+        return temp;
+    }
+
+    // Header 자료구조
+    private class _IP_Header {
+        byte ip_verlen; // ip version (1byte)   index 0
+        byte ip_tos; // type of service (1byte) index 1
+        short ip_len; // total packet length (2byte) index 2~3
+        short ip_id; // datagram Identification(2byte) index 4~5
+        short ip_fragoff;// fragment offset (2byte)	index 6~7
+        byte ip_ttl; // time to live in gateway hops (1byte) index 8
+        byte ip_proto; // IP protocol (1byte) index 9
+        short ip_cksum; // header checksum (2byte) index 10 11
+        // 0~11 index
+
+        _IP_ADDR ip_srcaddr;// source IP address (4byte) 12~15 index
+        _IP_ADDR ip_dstaddr;// destination IP address (4byte) 16~19 index
+
+        // byte ip_data[]; //variable length data (variable)
+
+        public _IP_Header() {
+            this.ip_verlen = 4;
+            this.ip_tos = 0;
+            this.ip_len = 0;
+            this.ip_id = 0;
+            this.ip_fragoff = 0;
+            this.ip_ttl = 0;
+            this.ip_proto = 0;
+            this.ip_cksum = 0;
+            this.ip_srcaddr = new _IP_ADDR();
+            this.ip_dstaddr = new _IP_ADDR();
+
+        }
+
+        // 헤더의 IP주소 자료구조
+        private class _IP_ADDR {
+            private byte[] addr = new byte[4];
+
+            public _IP_ADDR() {
+                this.addr[0] = 0;
+                this.addr[1] = 0;
+                this.addr[2] = 0;
+                this.addr[3] = 0;
+            }
+        }
+    }
+}
